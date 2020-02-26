a99fcdb390a36f47e7d0a8647186bb92e7a3ab84
Radina <radina_mihaylova0101@abv.bg>
2020-02-27 00:38:17 +0200

    options update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,43 +5,55 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
+    static String[][] secondOptionTableRows= new String[999][3];
+    static int rows = 0;
+    static int col = 0;
+
+    static String[] names = new String[999];
+
+    static int index = 0;
 
     public static void main(String[] args) {
-        System.out.println("------------------------------------\n" +
-                "   1. Заяви отпуска\n" +
-                "   2. Виж всички отпуски\n" +
-                "   3. Виж отпуска за служител\n" +
-                "   4. Промени статус на отпуска\n" +
-                "   5. Изход\n" +
-                "------------------------------------");
+        while(true) {
+            System.out.println("------------------------------------\n" +
+                    "   1. Заяви отпуска\n" +
+                    "   2. Виж всички отпуски\n" +
+                    "   3. Виж отпуска за служител\n" +
+                    "   4. Промени статус на отпуска\n" +
+                    "   5. Изход\n" +
+                    "------------------------------------");
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Choose one option: ");
-        byte chosenOption = input.nextByte();
-        switch (chosenOption) {
-            case 1:
-                enterDataAndCheckWhetherItIsTrue();
-                break;
-            case 2:
-                printTableForYourHolidays();
-                break;
-            case 3:
-                printTableForOtherEmployee();
-                break;
-            case 4:
-                System.out.println("");
-                break;
-            case 5:
-                exitProgram();
-                break;
-            default:
-                do {
-                    System.out.print("Въведената цифра не отговаря на нито една опция! " +
-                            "Моля, въведете пак:");
-                    chosenOption = input.nextByte();
-                } while (chosenOption > 5 || chosenOption < 1);
+            Scanner input = new Scanner(System.in);
+            System.out.print("Choose one option: ");
+            byte chosenOption = input.nextByte();
+            switch (chosenOption) {
+                case 1:
+                    enterDataAndCheckWhetherItIsTrue();
+                    break;
+                case 2:
+                    printTableForYourHolidays();
+                    break;
+                case 3:
+                    printTableForOtherEmployee();
+                    break;
+                case 4:
+                    System.out.println("");
+                    break;
+                case 5:
+                    exitProgram();
+                    break;
+                default:
+                    do {
+                        System.out.print("Въведената цифра не отговаря на нито една опция! " +
+                                "Моля, въведете пак:");
+                        chosenOption = input.nextByte();
+                    } while (chosenOption > 5 || chosenOption < 1);
+
+
+            }
         }
     }
 
@@ -78,33 +90,54 @@ public class Main {
     public static void enterDataAndCheckWhetherItIsTrue() {
         Scanner input = new Scanner(System.in);
         System.out.print("Име: ");
-        String name = input.next();
+        names[index] = input.next();
+        index++;
         System.out.print("Имейл: ");
         String email = input.next();
         System.out.print("ЕГН: ");
         String egn = input.next();
-        System.out.print("");
+        System.out.print("Вид на отпуската");
+        secondOptionTableRows[rows][col] = input.next();
+        col++;
+        System.out.println("start");
+        secondOptionTableRows[rows][col] = input.next();
+        col++;
+        System.out.println("end");
+        secondOptionTableRows[rows][col] = input.next();
+        col++;
+        if (col>=3){
+            col = 0;
+            rows++;
+        }
 
     }
 
     //TODO make the connection between the first and the second option and continue to fill the table with information
     //TODO learn how to make the array endless
-    //second Option
+    //secondOption
     public static void printTableForYourHolidays() {
-        String[][] secondOptionTableRows = {
-                {"1", "zdr", "zdr",},
-                {null, null, null, null},
-                {null, null, null, null}
-        };
         String[] columnNames = {"Заявки", "Начало(дата)", "Край(дата)"};
 
-        System.out.println(new doTable(secondOptionTableRows, columnNames));
+        new doTable(secondOptionTableRows, columnNames);
     }
 
     //thirdOption
     public static void printTableForOtherEmployee() {
-        String[][] thirdOptionTableRows = {};
-        String[] columnNames = {};
+        String[] columnNames = {"Заявки", "Начало(дата)", "Край(дата)"};
+        int request = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Въведи име на служител: ");
+        String nameOfEmployee = input.next();
+        String[][] employeeRequests = new String[rows][3];
+        for (int i = 0; i < names.length; i++) {
+            if (nameOfEmployee.equals(names[i])){
+                for (int j = 0; j < 3; j++) {
+                    employeeRequests[request][j] = secondOptionTableRows[i][j];
+                }
+                request++;
+            }
+        }
+        new doTable(employeeRequests, columnNames);
     }
 
     //TODO forth option
